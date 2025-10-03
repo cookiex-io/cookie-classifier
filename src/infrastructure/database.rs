45a97288189179a466b66eb4088b431d91477110
anuragRusty <anuragsingh2002dec@gmail.com>
2025-10-03 09:36:48 +0530

    Updated db name

--- src/infrastructure/database.rs
+++ src/infrastructure/database.rs
@@ -2,7 +2,7 @@ use std::env;
 use mongodb::{options::ClientOptions, Client};
 
 pub async fn get_client() -> mongodb::error::Result<Client> {
-    let db_uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
+    let db_uri = env::var("MONGODB_VCORE_URI").expect("MONGODB_VCORE_URI must be set");
     let client_options = ClientOptions::parse(db_uri).await?;
     let client = Client::with_options(client_options).expect("Failed to connect to MongoDB");
     Ok(client)
